92ca58ad52648be7f91a65cc338dc81d3bebdbe7
haraawrrr <riefka_azka@sbm-itb.ac.id>
2024-11-07 22:03:12 +0700

    Add files via upload

---
+++ Olahdata1.R
@@ -0,0 +1,4 @@
+data <- read.csv("C:/Users/Riefka Hasna/Downloads/archive (3)/train.csv")
+head(data)
+
+summary(data)
---
+++ Olahdata2.R
@@ -0,0 +1,8 @@
+data <- read.csv("C:/Users/Riefka Hasna/Downloads/archive (3)/train.csv")
+head(data)
+
+data$Order.Date <- as.Date(data$Order.Date, format = "%Y-%m-%d")
+
+sorted_data <- data[order(data$Order.Date), ]
+
+head(data)
